14cc92f35470025e8fe9cd74da11b1639dcf462a
Ivan Scherbak <dev@funivan.com>
2015-03-07 22:02:35 +0200

    prototype method call

--- tests/Tokenizer/ExtractorTest.php
+++ tests/Tokenizer/ExtractorTest.php
@@ -7,23 +7,22 @@
    */
   class ExtractorTest extends \PHPUnit_Framework_TestCase {
 
-    public function _testExtractor(){
+    public function _testExtractor() {
 
       $extractor = ClassExtractor::create()->find(
         ClassMethodDeclaration::create()
-         ->nameLike('^action.*$')
-         ->find(
-          TokenSquence::create()
-            ->strict()->valueIs('header')
-            ->strict()->valueIs('(')
-            ->strict()->valueLike('!^\s*location\s*:!i')
-            ->search()->valueIs(';')
-         )
+          ->nameLike('^action.*$')
+          ->find(
+            TokenSquence::create()
+              ->strict()->valueIs('header')
+              ->strict()->valueIs('(')
+              ->strict()->valueLike('!^\s*location\s*:!i')
+              ->search()->valueIs(';')
+          )
       );
 
       $blocks = $extractor->extract($collection, 'function');
       $blocks;
-
 
       $extractor = ClassExtractor::create()->find(
         MethodExtractor::create()
@@ -43,7 +42,6 @@
         ->strict()->valueLike('.*')
         ->search()->valueIs("{");
 
-
       $extractor = ClassExtractor::create()->find(
         ClassConstantDeclaration::create()
       );
@@ -58,19 +56,82 @@
         ->strict()->typeIs(T_WHITESPACE)
         ->strict()->valueIs('return');
 
-
       ClassMethodDeclaration::create()
         ->find(
-        TokenSequence::create()
-          ->strict()->valueIs('if')
-          ->section('(',')')->filter(
-            FunctionCall::create()
-              ->name('empty')
-              ->argument(0, '^$_(GET|POST)')
-
-          )
+          TokenSequence::create()
+            ->strict()->valueIs('if')
+            ->section('(', ')')->filter(
+              FunctionCall::create()
+                ->name('empty')
+                ->argument(0, '^$_(GET|POST)')
+            )
 
         );
+
+      // Atl_Response::redirectReferer('/');
+      // $this->::redirectReferer('/')
+      // find only in specific classes 
+      $extractor = ClassExtractor::create()
+        ->extendClass('AdminController')
+        ->find(
+          TokenSequence::create()
+            ->strict()->valueIs('Atl_Response')
+            ->strict()->valueIs('::')
+            ->strict()->valueIs('redirectReferer')
+
+        );
+
+      $extractor->find()->map(function (\Funivan\PhpTokenizer\Collection $collection) {
+        $collection->removeWhitespaces();
+        $collection->refresh();
+
+        $collection[0]->setValue('$this->');
+        $collection[1]->remove();
+      });
+
+      // $this->response()->redirect('/orders/order_list');
+      // $this->redirect('/orders/order_list');
+
+    }
+
+
+    public function _testMethodCallFinder() {
+
+      # 1. simple method call extractor
+      $extractor = MethodCall::create()
+        ->objectVariable('$this')
+        ->method('response')
+        ->method('redirect');
+
+      // $this->response()->redirect();
+      // $this->response([1,2,3])->redirect([0], 123);
+
+      # 2. static method on object  
+      $extractor = MethodCall::create()
+        ->objectVariable('$this')
+        ->staticMethod('response');
+
+      // $this::redirect();
+
+      # 2.1. Static method 
+      $extractor = MethodCall::create()
+        ->className('UserName')->staticMethod('response');
+
+      // UserName::redirect();
+
+      # 3. With method filter
+      $extractor = MethodCall::create()
+        ->objectVariable('$this')
+        ->method(
+          MethodCall::create()
+            ->name('response')
+            ->withArgument(0, '!$.*!')
+            ->withArgumentsNum(3)
+        );
+      
+      
+      
+      
 
     }
 
