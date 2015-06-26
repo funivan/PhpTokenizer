9b404c936674fc8cde5fe7aa2384f44514da921f
Ivan Scherbak <dev@funivan.com>
2015-06-26 07:11:35 +0300

    test: find properties in class

--- tests/Tokenizer/TokensCheckerTest.php
+++ tests/Tokenizer/TokensCheckerTest.php
@@ -3,6 +3,7 @@
   namespace Test\Funivan\PhpTokenizer\Tokenizer;
 
   use Funivan\PhpTokenizer\Collection;
+  use Funivan\PhpTokenizer\Strategy\Possible;
   use Funivan\PhpTokenizer\Strategy\Strict;
   use Funivan\PhpTokenizer\StreamProcess\StreamProcess;
   use Funivan\PhpTokenizer\StreamProcess\TokensChecker;
@@ -45,27 +46,52 @@
       $tokensChecker->pattern(new ClassPattern());
 
       $this->assertCount(2, $tokensChecker->getCollections());
-      
+
       $tokensChecker = new TokensChecker(Collection::initFromString($code));
       $classPattern = new ClassPattern();
       $classPattern->nameIs('B');
       $tokensChecker->pattern($classPattern);
 
       $this->assertCount(0, $tokensChecker->getCollections());
-      
+
       $tokensChecker = new TokensChecker(Collection::initFromString($code));
       $classPattern = new ClassPattern();
       $classPattern->nameIs('A');
       $tokensChecker->pattern($classPattern);
 
       $this->assertCount(1, $tokensChecker->getCollections());
-      
+
       $tokensChecker = new TokensChecker(Collection::initFromString($code));
       $classPattern = new ClassPattern();
       $classPattern->nameIs('customUser');
       $tokensChecker->pattern($classPattern);
 
       $this->assertCount(1, $tokensChecker->getCollections());
+    }
+
+    public function testWithNestedPatterns() {
+      # find class with property 
+      $code = '<? class A { public $user = null; } class customUser { }';
+      $tokensChecker = new TokensChecker(Collection::initFromString($code));
+      $tokensChecker->pattern(new ClassPattern())
+        ->pattern(function (StreamProcess $process) {
+
+          $result = array();
+          foreach ($process as $p) {
+            $p->process(Strict::create()->valueIs(array('public', 'protected', 'private')));
+            $p->process(Possible::create()->valueIs(array('static')));
+            $name = $p->strict(T_VARIABLE);
+            if ($p->isValid()) {
+              $result[] = $name;
+            }
+
+          }
+
+          return $result;
+        });
+
+      $this->assertCount(1, $tokensChecker->getCollections());
+
     }
 
   }
