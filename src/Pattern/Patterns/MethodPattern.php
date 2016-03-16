a6414e1cec2f42b48c5e3773e9cfe01576f1bcfa
Ivan Scherbak <dev@funivan.com>
2016-03-16 13:02:32 +0200

    Improve method pattern

--- src/Pattern/Patterns/MethodPattern.php
+++ src/Pattern/Patterns/MethodPattern.php
@@ -6,23 +6,90 @@
   use Funivan\PhpTokenizer\QuerySequence\QuerySequence;
   use Funivan\PhpTokenizer\Strategy\QueryStrategy;
   use Funivan\PhpTokenizer\Strategy\Strict;
+  use Funivan\PhpTokenizer\Token;
 
   /**
    * Find method in code
    */
   class MethodPattern implements PatternInterface {
 
+    const OUTPUT_BODY = 0;
+
+    const OUTPUT_FULL = 1;
+
+    const OUTPUT_DOC_COMMENT = 2;
+
+
     /**
      * @var QueryStrategy
      */
     private $nameQuery;
 
+    /**
+     * @var callable[]
+     */
+    private $modifierChecker = [];
 
     /**
-     * MethodPattern constructor.
+     * @var callable
+     */
+    private $bodyChecker;
+
+    /**
+     * @var callable
+     */
+    private $docCommentChecker;
+
+    /**
+     * @var int
+     */
+    private $outputType = self::OUTPUT_BODY;
+
+
+    /**
+     *
      */
     public function __construct() {
-      $this->nameQuery = Strict::create()->valueLike('!.+!');
+      $this->withName(Strict::create()->valueLike('!.+!'));
+      $this->withAnyModifier();
+
+      /** @noinspection PhpUnusedParameterInspection */
+      $this->withBody(function (Collection $body) {
+        return true;
+      });
+
+      /** @noinspection PhpUnusedParameterInspection */
+      $this->withDocComment(function (Token $token) {
+        return true;
+      });
+
+    }
+
+
+    /**
+     * @return $this
+     */
+    public function outputFull() {
+      $this->outputType = self::OUTPUT_FULL;
+      return $this;
+    }
+
+
+    /**
+     * @return $this
+     */
+    public function outputBody() {
+      $this->outputType = self::OUTPUT_BODY;
+      return $this;
+    }
+
+
+    /**
+     * @return $this
+     */
+    public function outputDocComment() {
+      $this->outputType = self::OUTPUT_DOC_COMMENT;
+      return $this;
     }
 
 
@@ -44,22 +111,233 @@
 
 
     /**
+     * @param callable $check
+     * @return $this
+     */
+    public function withBody(callable $check) {
+      $this->bodyChecker = $check;
+      return $this;
+    }
+
+
+    /**
+     * @param Collection $body
+     * @return bool
+     * @throws \Exception
+     */
+    private function isValidBody(Collection $body) {
+      $checker = $this->bodyChecker;
+      $result = $checker($body);
+      if (!is_bool($result)) {
+        throw new \Exception('Body checker should return boolean result');
+      }
+
+      return $result;
+    }
+
+
+    /**
+     * @param callable $check
+     * @return $this
+     */
+    public function withDocComment(callable $check = null) {
+
+      if ($check === null) {
+        $check = function (Token $token) {
+          return $token->getType() === T_DOC_COMMENT;
+        };
+      }
+      $this->docCommentChecker = $check;
+
+      return $this;
+    }
+
+
+    /**
+     * Find functions without doc comments
+     */
+    public function withoutDocComment() {
+      $this->docCommentChecker = function (Token $token) {
+        return $token->isValid() === false;
+      };
+
+      return $this;
+    }
+
+
+    /**
+     * @return $this
+     */
+    public function withAnyModifier() {
+      $this->modifierChecker = [];
+      $this->modifierChecker[] = function () {
+        return true;
+      };
+      return $this;
+    }
+
+
+    /**
+     * @param string $modifier
+     * @return $this
+     */
+    public function withModifier($modifier) {
+      $this->modifierChecker[] = function ($allModifiers) use ($modifier) {
+        return in_array($modifier, $allModifiers);
+      };
+
+      return $this;
+    }
+
+
+    /**
+     * @param string $modifier
+     * @return $this
+     */
+    public function withoutModifier($modifier) {
+
+      $this->modifierChecker[] = function ($allModifiers) use ($modifier) {
+        return !in_array($modifier, $allModifiers);
+      };
+
+      return $this;
+    }
+
+
+    /**
+     * @param array $modifiers Array<String>
+     * @return bool
+     * @throws \Exception
+     */
+    private function isValidModifiers(array $modifiers) {
+      foreach ($this->modifierChecker as $checkModifier) {
+        $result = $checkModifier($modifiers);
+
+        if (!is_bool($result)) {
+          throw new \Exception('Modifier checker should return boolean result');
+        }
+        if ($result === false) {
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+
+    /**
      * @param QuerySequence $querySequence
      * @return Collection|null
      */
     public function __invoke(QuerySequence $querySequence) {
+      static $availableModifiers = [
+        T_STATIC,
+        T_PRIVATE,
+        T_PUBLIC,
+        T_ABSTRACT,
+        T_FINAL,
+      ];
 
-      $querySequence->strict('function');
+
+      # detect function
+      $functionKeyword = $querySequence->strict('function');
       $querySequence->strict(T_WHITESPACE);
       $querySequence->process($this->nameQuery);
       $querySequence->section('(', ')');
+      $querySequence->possible(T_WHITESPACE);
       $body = $querySequence->section('{', '}');
 
-      if ($querySequence->isValid()) {
-        return $body->extractItems(1, -1);
+      if (!$querySequence->isValid()) {
+        return null;
       }
 
-      return null;
+      $collection = $querySequence->getCollection();
+      $start = $collection->extractByTokens($collection->getFirst(), $functionKeyword);
+      $start->slice(0, -1);  // remove last function keyword
+
+      # start reverse search
+      $items = array_reverse($start->getItems());
+      $startFrom = null;
+
+      $docComment = new Token();
+
+      $modifiers = [];
+
+
+      /** @var Token[] $items */
+      foreach ($items as $item) {
+
+        if ($item->getType() === T_WHITESPACE) {
+          $startFrom = $item;
+          continue;
+        }
+
+        if ($item->getType() === T_DOC_COMMENT and $docComment->isValid() === false) {
+          # Detect only first doc comment
+          $startFrom = $item;
+          $docComment = $item;
+          continue;
+        }
+
+
+        if (in_array($item->getType(), $availableModifiers)) {
+          $startFrom = $item;
+          $modifiers[] = $item->getValue();
+          continue;
+        }
+
+        break;
+      }
+
+      if ($this->isValidModifiers($modifiers) === false) {
+        return null;
+      }
+
+      if ($this->isValidBody($body) === false) {
+        return null;
+      }
+
+      if ($this->isValidDocComment($docComment) === false) {
+        return null;
+      }
+
+      if (is_null($startFrom)) {
+        $startFrom = $functionKeyword;
+      }
+
+
+      if ($this->outputType === self::OUTPUT_FULL) {
+        # all conditions are ok, so extract full function
+        $fullFunction = $collection->extractByTokens($startFrom, $body->getLast());
+        if ($fullFunction->getFirst()->getType() == T_WHITESPACE) {
+          $fullFunction->slice(1);
+        }
+        return $fullFunction;
+
+      } elseif ($this->outputType == self::OUTPUT_DOC_COMMENT) {
+        return new Collection([$docComment]);
+      }
+
+      # body by default
+      $body->slice(0, -1);
+      return $body;
     }
+
+
+    /**
+     * @param Token $token
+     * @return mixed
+     * @throws \Exception
+     */
+    private function isValidDocComment(Token $token) {
+      $checker = $this->docCommentChecker;
+      $result = $checker($token);
+      if (!is_bool($result)) {
+        throw new \Exception('DocComment checker should return boolean result');
+      }
+
+      return $result;
+    }
+
 
   }
--- tests/Tokenizer/Patterns/MethodPatternTest.php
+++ tests/Tokenizer/Patterns/MethodPatternTest.php
@@ -5,7 +5,9 @@
   use Funivan\PhpTokenizer\Collection;
   use Funivan\PhpTokenizer\Pattern\Pattern;
   use Funivan\PhpTokenizer\Pattern\Patterns\MethodPattern;
+  use Funivan\PhpTokenizer\Query\Query;
   use Funivan\PhpTokenizer\Strategy\Strict;
+  use Funivan\PhpTokenizer\Token;
   use Test\Funivan\PhpTokenizer\MainTestCase;
 
   /**
@@ -47,6 +49,198 @@
       $tokensChecker = new Pattern(Collection::createFromString($code));
       $this->assertCount(1, $tokensChecker->apply($pattern)->getCollections());
 
+    }
+
+
+    public function testMatchWithEmptyStartFrom() {
+
+      $code = '<?php function test(){
+
+      }
+
+      ';
+
+      $tokensChecker = new Pattern(Collection::createFromString($code));
+      $tokensChecker->apply((new MethodPattern()));
+      $this->assertCount(1, $tokensChecker->getCollections());
+
+    }
+
+
+    public function testMatchWithMultipleKeywords() {
+
+      $code = '<?php
+
+      public static final function test(){
+
+      }
+
+      public function test(){
+
+      }
+
+      function test(){
+
+      }
+
+      ';
+
+      $tokensChecker = new Pattern(Collection::createFromString($code));
+      $tokensChecker->apply((new MethodPattern()));
+      $this->assertCount(3, $tokensChecker->getCollections());
+
+
+      $tokensChecker = new Pattern(Collection::createFromString($code));
+      $tokensChecker->apply((new MethodPattern())->withModifier('static'));
+      $this->assertCount(1, $tokensChecker->getCollections());
+
+      $tokensChecker = new Pattern(Collection::createFromString($code));
+      $tokensChecker->apply((new MethodPattern())->withModifier('public'));
+      $this->assertCount(2, $tokensChecker->getCollections());
+
+
+      $tokensChecker = new Pattern(Collection::createFromString($code));
+      $tokensChecker->apply((new MethodPattern())->withoutModifier('public'));
+      $this->assertCount(1, $tokensChecker->getCollections());
+
+    }
+
+
+    public function testDocCommentMatch() {
+
+      $code = '<?php
+     /**
+      * hello
+      */
+      public static final function test0(){
+         echo "test0";
+      }
+
+
+      /**
+       * hello test
+       */
+      public function test1(){
+         echo "test1";
+      }
+
+      public function test2(){
+        echo "test2";
+      }
+
+      ';
+
+      $tokensChecker = new Pattern(Collection::createFromString($code));
+      $tokensChecker->apply((new MethodPattern()));
+      $this->assertCount(3, $tokensChecker->getCollections());
+
+      $tokensChecker = new Pattern(Collection::createFromString($code));
+      $tokensChecker->apply((new MethodPattern())->withDocComment());
+      $this->assertCount(2, $tokensChecker->getCollections());
+
+
+      $tokensChecker = new Pattern(Collection::createFromString($code));
+      $tokensChecker->apply((new MethodPattern())->withoutDocComment());
+      $this->assertCount(1, $tokensChecker->getCollections());
+
+
+      $tokensChecker = new Pattern(Collection::createFromString($code));
+      $tokensChecker->apply((new MethodPattern())->withDocComment(function (Token $token) {
+        return (boolean) preg_match('!hello!', $token->getValue());
+      }));
+
+      $this->assertCount(2, $tokensChecker->getCollections());
+
+      $tokensChecker = new Pattern(Collection::createFromString($code));
+      $tokensChecker->apply((new MethodPattern())->withDocComment(function (Token $token) {
+        return (boolean) preg_match('!test!', $token->getValue());
+      }));
+
+      $this->assertCount(1, $tokensChecker->getCollections());
+
+    }
+
+
+    public function testBodyFiler() {
+
+      $code = '<?php
+
+      public static final function test0(){
+         echo "test0";
+      }
+
+      public function test1(){
+         echo "test11";
+      }
+
+      public function test2(){
+        echo "test22";
+      }
+
+      ';
+
+      $tokensChecker = new Pattern(Collection::createFromString($code));
+      $tokensChecker->apply((new MethodPattern()));
+      $this->assertCount(3, $tokensChecker->getCollections());
+
+      $tokensChecker = new Pattern(Collection::createFromString($code));
+      $tokensChecker->apply((new MethodPattern())->withBody(function (Collection $collection) {
+        return $collection->find((new Query())->typeIs(T_ECHO))->count() > 0;
+      }));
+
+      $tokensChecker = new Pattern(Collection::createFromString($code));
+      $tokensChecker->apply((new MethodPattern())->withBody(function (Collection $collection) {
+        return $collection->find((new Query())->valueLike('!test22!'))->count() == 1;
+      }));
+
+      $this->assertCount(1, $tokensChecker->getCollections());
+
+    }
+
+
+    public function testOutput() {
+      $code = '<?php
+             /**
+              * comment
+              */
+      public static final function test0(){
+         echo "test0";
+      }
+
+      ';
+
+      $tokensChecker = new Pattern(Collection::createFromString($code));
+      $tokensChecker->apply((new MethodPattern()));
+      $collections = $tokensChecker->getCollections();
+      $this->assertCount(1, $collections);
+      $this->assertContains('echo "test0";', (string) $collections[0]);
+
+      $tokensChecker = new Pattern(Collection::createFromString($code));
+      $tokensChecker->apply((new MethodPattern())->outputBody());
+      $collections = $tokensChecker->getCollections();
+      $this->assertCount(1, $collections);
+      $this->assertContains('echo "test0";', (string) $collections[0]);
+
+
+      $tokensChecker = new Pattern(Collection::createFromString($code));
+      $tokensChecker->apply((new MethodPattern())->outputFull());
+
+      $collections = $tokensChecker->getCollections();
+      $this->assertCount(1, $collections);
+      $this->assertContains('* comment', (string) $collections[0]);
+      $this->assertContains('public static final function test0(){', (string) $collections[0]);
+      $this->assertContains('echo "test0";', (string) $collections[0]);
+
+
+      $tokensChecker = new Pattern(Collection::createFromString($code));
+      $tokensChecker->apply((new MethodPattern())->outputDocComment());
+
+      $collections = $tokensChecker->getCollections();
+      $this->assertCount(1, $collections);
+      $this->assertContains('* comment', (string) $collections[0]);
+
+      $comment = Collection::createFromString($code)->find((new Query())->typeIs(T_DOC_COMMENT))->getFirst();
+      $this->assertEquals($comment->getValue(), (string) $collections[0]);
     }
 
 
